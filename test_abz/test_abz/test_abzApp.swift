71e154fa4683ee33777e1003b956ca991322b3ec
flowereDnap <nikitashapko5@gmail.com>
2024-08-19 23:37:32 +0300

    basic

---
+++ test_abz/test_abz/test_abzApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_abzApp.swift
+//  test_abz
+//
+//  Created by mac on 19.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct test_abzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
