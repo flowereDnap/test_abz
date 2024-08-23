4ab0d5c2e7c5931b8c2c4eaac27f22be3eb48c6f
flowereDnap <nikitashapko5@gmail.com>
2024-08-23 13:18:33 -0400

    Update signUpView.swift

--- test_abz/test_abz/Views/views/signUpView.swift
+++ test_abz/test_abz/Views/views/signUpView.swift
@@ -13,6 +13,11 @@ struct SignupView: View {
     @State var text1: String = ""
     @State var text2: String = ""
     @State var text3: String = ""
+    @State private var isPickerPresented: Bool = false
+    @State private var selectedItem: PhotosPickerItem? = nil
+
+    var validateImage : (UIImage, Int) -> Bool
+
     var nameFieldValidation: (String) -> (Bool, String) = { return (!$0.isEmpty , "Required field")}
     
     var emailFieldValidation: (String) -> (Bool, String) = { if $0.isEmpty {
@@ -36,18 +41,18 @@ struct SignupView: View {
         VStack( spacing: 24){
             VStack(spacing: 32) {
                 CustomTextField(
-                    validate: nameFieldValidation,
+                    isValid: nameValid,
                     placeholder:  "Your name",
                     text: $text1
                 )
                 CustomTextField(
-                    validate: emailFieldValidation,
+                    isValid: emailValid,
                     placeholder:  "Email",
                     text: $text2
                 )
                 CustomTextField(
                     subText: "+38 (XXX) XXX - XX - XX",
-                    validate: phoneFieldValidation,
+                    isValid: phoneValid,
                     placeholder:  "Phone",
                     text: $text3
                 )
@@ -63,22 +68,36 @@ struct SignupView: View {
                 
             ZStack {
                 CustomTextField(
-                    validate: nameFieldValidation,
+                    isValid: $imageValid,
                     placeholder:  "Your name",
                     text: $text1
                 )
             }
                 //TODO picker
-                Button {
-                    
-                } label: {
-                    Text("Sign Up")
-                }.buttonStyle(PrimaryButtonStyle())
+            Button("Select Photo") {
+                isPickerPresented = true
+            }
+            .photosPicker(isPresented: $isPickerPresented, selection: $selectedItem, matching: .images)
+            .buttonStyle(PrimaryButtonStyle())
                 
                 Spacer()
             }
             .padding(.vertical, 32)
             .padding(.horizontal, 16)
+.onChange(of: selectedItem) { newItem in
+            Task {
+                if let data = try? await newItem?.loadTransferable(type: Data.self),
+                   let uiImage = UIImage(data: data) {
+                    if validateImage(uiImage, dataSize: data.count) {
+                        selectedImage = uiImage
+                        imageValid
+                    } else {
+                        selectedImage = nil
+                        imageValid = false
+                    }
+                }
+            }
+        }
         }
     }
     
